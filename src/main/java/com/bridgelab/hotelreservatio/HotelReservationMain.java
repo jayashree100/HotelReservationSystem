64d22e2200d7789f8ec41d18481267fa7bd2e557
surekhakallutla <surekhakallutla@gmail.com>
2022-04-12 10:27:30 +0530

    first Commit

---
+++ src/main/java/com/bridgelab/hotelreservatio/HotelReservationMain.java
@@ -0,0 +1,9 @@
+package com.bridgelab.hotelreservatio;
+
+public class HotelReservationMain {
+	public static void main(String[] args) {
+		System.out.println("WelCome To HotelReservation System");
+		
+
+	}
+}
